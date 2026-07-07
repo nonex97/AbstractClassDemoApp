9b9ea46a99b2b6c3c9948822729aacc8d4a91fbb
nonex97 <ngrguric97@gmail.com>
2026-07-07 14:07:55 +0200

    Learned about abstract classes and why are they used.

---
+++ AbstractClassDemo/InventoryItem.cs
@@ -0,0 +1,8 @@
+﻿namespace AbstractClassDemo
+{
+    public abstract class InventoryItem // abstract class is a base class, you build upon it, but it's not a fully formed class. it can't be instantiated
+    {
+        public string ProductName { get; set; }
+        public int QuantityOnHand { get; set; }
+    }
+}
--- AbstractClassDemo/Program.cs
+++ AbstractClassDemo/Program.cs
@@ -10,8 +10,25 @@ namespace AbstractClassDemo
     {
         static void Main(string[] args)
         {
+            InventoryItem book = new Book(); 
+            book.ProductName = "A tale of Two Cities";
 
+            Car car = new Car();
+            
             Console.ReadLine();
         }
+    }
+
+    public abstract class Vehicle
+    {
+        public string VIN { get; set; }
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+        public int YearManufactured { get; set; }
+    }
+
+    public class Car : Vehicle
+    {
+        public int NumberOfWheels { get; set; } = 4;
     }
 }
