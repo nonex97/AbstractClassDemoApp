053926d9d2de06503c250ed1e9cd4d5c3a44d63c
nonex97 <ngrguric97@gmail.com>
2026-07-07 14:18:41 +0200

    Homework: Create an Abstract Cals that has an Interface applied to it.

--- AbstractClassDemo/Program.cs
+++ AbstractClassDemo/Program.cs
@@ -14,6 +14,13 @@ namespace AbstractClassDemo
             book.ProductName = "A tale of Two Cities";
 
             Car car = new Car();
+
+            Ubahn U3 = new Ubahn();
+            U3.UbahnLine = "U3";
+            U3.TypeOfPublicTransport = "Subway";
+
+            PublicTransport U6 = new Ubahn();
+            U6.TypeOfPublicTransport = "Subway";
             
             Console.ReadLine();
         }
@@ -30,5 +37,25 @@ namespace AbstractClassDemo
     public class Car : Vehicle
     {
         public int NumberOfWheels { get; set; } = 4;
+    }
+
+    public abstract class PublicTransport : IFuel
+    {
+        public string TypeOfPublicTransport { get; set; }
+
+        public void StartFueling()
+        {
+            // fueling for any type, but can be different
+        }
+    }
+
+    public interface IFuel
+    {
+        void StartFueling();
+    }
+
+    public class Ubahn : PublicTransport
+    {
+        public string UbahnLine { get; set; }
     }
 }
